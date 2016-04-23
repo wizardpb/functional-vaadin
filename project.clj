f1ae6c878c9931d7f3a4783d15cd0c54649dd1bf
Paul Bennett <paul@prajnainc.com>
2016-04-23 17:48:02 -0400

    Added group-id and README.ms updates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject functional-vaadin "0.1.0-SNAPSHOT"
+(defproject com.prajnainc/functional-vaadin "0.1.0-SNAPSHOT"
   :description "A functional interface to Vaadin"
   ;:url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
