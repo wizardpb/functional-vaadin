7c4194f8339825b7c1264733ff1bd39e518ccb33
Paul Bennett <paul@prajnainc.com>
2016-05-02 19:13:55 -0400

    Get *all* the tests to run

---
+++ test/functional_vaadin/data_binding/binding_test.clj
@@ -0,0 +1,5 @@
+(ns functional-vaadin.data-binding.binding-test
+  (:require [clojure.test :refer :all]
+            [functional-vaadin.data-binding.binding :refer :all])
+  (:import [com.vaadin.ui TextField]))
+
