b02008807e7b01bab6f0d90d201d2be356a9a4b7
Paul Bennett <paul@prajnainc.com>
2016-05-17 15:22:26 -0400

    Updated generated dependencies

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
-(defproject com.prajnainc/functional-vaadin "0.1.0"
+(defproject com.prajnainc/functional-vaadin "0.1.1"
   :description "A functional interface to Vaadin"
   :url "https://github.com/wizardpb/functional-vaadin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-codox "0.9.5"]
             [lein-pprint "1.1.1"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [io.reactivex/rxclojure "1.0.0"]]
   :codox {:namespaces [functional-vaadin.core
                        functional-vaadin.conversion
                        functional-vaadin.rx.observers
                        functional-vaadin.rx.operators
                        functional-vaadin.examples.Sampler]}
-  :profiles {:provided {:dependencies [[io.reactivex/rxclojure "1.0.0"]
-                                       [com.vaadin/vaadin-server "7.6.5"]
+  :profiles {:provided {:dependencies [[com.vaadin/vaadin-server "7.6.5"]
                                        [com.vaadin/vaadin-client-compiled "7.6.5"]
                                        [com.vaadin/vaadin-themes "7.6.5"]
                                        [org.eclipse.jetty/jetty-server "9.3.8.v20160314"]
@@ -28,15 +28,15 @@
                                        [org.clojure/tools.namespace "0.2.11"]
                                        ]
                         }
-             :uberjar {:aot [functional-vaadin.examples.Sampler]
-                       :main functional-vaadin.examples.run
-                       :dependencies [[io.reactivex/rxclojure "1.0.0"]
-                                      [com.vaadin/vaadin-server "7.6.5"]
-                                      [com.vaadin/vaadin-client-compiled "7.6.5"]
-                                      [com.vaadin/vaadin-themes "7.6.5"]
-                                      [org.eclipse.jetty/jetty-server "9.3.8.v20160314"]
-                                      [org.eclipse.jetty/jetty-servlet "9.3.8.v20160314"]
-                                      [javax.servlet/javax.servlet-api "3.1.0"]]
-                   }
+             :uberjar  {:aot [functional-vaadin.examples.Sampler]
+                        :main functional-vaadin.examples.run
+                        :dependencies [[io.reactivex/rxclojure "1.0.0"]
+                                       [com.vaadin/vaadin-server "7.6.5"]
+                                       [com.vaadin/vaadin-client-compiled "7.6.5"]
+                                       [com.vaadin/vaadin-themes "7.6.5"]
+                                       [org.eclipse.jetty/jetty-server "9.3.8.v20160314"]
+                                       [org.eclipse.jetty/jetty-servlet "9.3.8.v20160314"]
+                                       [javax.servlet/javax.servlet-api "3.1.0"]]
+                        }
              }
   )
